c6803ce8894f8883c4c0478bd01a9ca92e0dbc48
EricVeen <ericveen@hotmail.nl>
2020-01-23 11:17:10 +0100

    current

--- MultiMine/MultiMineServer/Server.cs
+++ MultiMine/MultiMineServer/Server.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultiMine.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,9 +7,13 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
 
-namespace MultiMineServer {
-    public class Server {
+namespace MultiMineServer
+{
+    public class Server
+    {
 
         TcpListener listener;
         private List<Client> clients = new List<Client>();
@@ -16,24 +21,24 @@ namespace MultiMineServer {
         internal List<Client> Clients { get => clients; set => clients = value; }
 
         public Server()
-        { 
+        {
             StartServer();
 
             Console.WriteLine("Server started and listening.");
             Console.WriteLine("Type quit to exit.");
 
-             bool running = true;
-             while (running)
+            bool running = true;
+            while (running)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "quit")
                 {
-             string userInput = Console.ReadLine();
-             if (userInput.ToLower() == "quit")
-             {
-                  running = false;
-              }
-              else
-               {
-                   Console.WriteLine("Unknown command: {0}", userInput);
-             }
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command: {0}", userInput);
+                }
             }
         }
 
@@ -51,7 +56,7 @@ namespace MultiMineServer {
             tcpHandlerThread.Start(tcpClient);
             Console.WriteLine("New client connected");
             Console.WriteLine(tcpClient.GetStream());
-            
+
             clients.Add(new Client(tcpClient, this));
 
             //listener.BeginAcceptTcpClient(new AsyncCallback(OnConnect), null);
@@ -64,15 +69,32 @@ namespace MultiMineServer {
             byte[] message = new byte[1024];
             while (true)
             {
-                stream.Read(message,0,message.Length);
+                stream.Read(message, 0, message.Length);
             }
             stream.Close();
             mclient.Close();
         }
 
-        private void writeToFile()
+        private void writeToFile(GameBoard gameBoard)
         {
+            string output = JsonConvert.SerializeObject(gameBoard);
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(getPath() + "Data/Data.txt", true))
+            {
+                file.WriteLine(output);
+            }
+        }
 
+        private GameBoard readFromFile()
+        {
+            return (GameBoard)JsonConvert.DeserializeObject(System.IO.File.ReadAllText(getPath() + "Data/Data.txt"));
+        }
+        public static string getPath()
+        {
+            string startupPath = System.IO.Directory.GetCurrentDirectory();
+            string Startsplit = startupPath.Substring(0, startupPath.LastIndexOf("bin"));
+            string split = Startsplit.Replace(@"\", "/");
+            return split;
         }
 
     }
